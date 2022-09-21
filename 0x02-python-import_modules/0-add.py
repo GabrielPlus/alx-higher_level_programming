ef6340cf4a06d3516b7ce1311b9fec7781f2270a
pius2001 <gabrielpius0311@gmail.com>
2022-09-21 19:13:55 +0000

    added

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 0_add.py
+#pius2001 
+
+if _import__ == "_main_":
+    """print the sum of 1 and 2."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{} + {} = {}".format(a,b, add(a, b)))
